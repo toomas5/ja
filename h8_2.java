a108cebb06c8a7cf3aef6e34d194292b72c21666
toomas5 <115462520+toomas5@users.noreply.github.com>
2023-10-06 08:56:46 +0300

    Add files via upload

--- h8_2.java
+++ h8_2.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 public class h8_2 {
 
     public static void main(String[] args) {
-        h821 kiisu = new h821("Mart", 35, "töötu", "eesti");
-        kiisu.tüüp();
-        kiisu.nimi();
-        kiisu.vanus();
-        kiisu.riik();
+        h821 ee = new h821("Mart", 35, "töötu", "eesti");
+        ee.tüüp();
+        ee.nimi();
+        ee.vanus();
+        ee.riik();
         System.out.println("");
         ;
         h822 arb = new h822("tööl", "Aruba", "avamerel", "ülejäänud meeskonnaga");
